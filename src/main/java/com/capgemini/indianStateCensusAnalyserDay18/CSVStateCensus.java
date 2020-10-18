0230df34a038ec63305bc970e398931cd74cd292
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-19 00:40:18 +0530

    Refactored to ensure single responsibilty

--- src/main/java/com/capgemini/indianStateCensusAnalyserDay18/CSVStateCensus.java
+++ src/main/java/com/capgemini/indianStateCensusAnalyserDay18/CSVStateCensus.java
@@ -22,7 +22,8 @@ public class CSVStateCensus {
 		checkHeader(csvFilePath);
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-			Iterator<IndianCensusCSV> censusCsvIterator = getCSVFileIterator(reader, IndianCensusCSV.class);
+			Iterator<IndianCensusCSV> censusCsvIterator = new OpenCSVBuilder().getCSVFileIterator(reader,
+					IndianCensusCSV.class);
 			return this.getCount(censusCsvIterator);
 
 		} catch (IOException e) {
@@ -73,7 +74,8 @@ public class CSVStateCensus {
 		checkHeaderStateCode(csvFilePath);
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-			Iterator<IndianStateCodeCSV> censusCsvIterator = getCSVFileIterator(reader, IndianStateCodeCSV.class);
+			Iterator<IndianStateCodeCSV> censusCsvIterator = new OpenCSVBuilder().getCSVFileIterator(reader,
+					IndianStateCodeCSV.class);
 			return this.getCount(censusCsvIterator);
 		} catch (IOException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.WRONG_FILE_PATH);
@@ -106,19 +108,6 @@ public class CSVStateCensus {
 			}
 
 		} catch (NullPointerException | IOException e) {
-		}
-
-	}
-
-	private <E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass) throws CensusAnalyserException {
-		try {
-			CsvToBeanBuilder<E> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
-			csvToBeanBuilder.withType(csvClass);
-			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
-			CsvToBean<E> csvToBean = csvToBeanBuilder.build();
-			return csvToBean.iterator();
-		} catch (IllegalStateException e) {
-			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.WRONG_FILE_TYPE);
 		}
 
 	}
